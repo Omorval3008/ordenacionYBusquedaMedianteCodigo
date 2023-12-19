4335f7161e533585b3b8cc38bf25a31bcc79f547
Omorval3008 <omorval3008@g.educaand.es>
2023-12-19 12:40:13 +0100

    Todos los metodos estan implementados

--- src/main/java/daw/EJ1.java
+++ src/main/java/daw/EJ1.java
@@ -16,74 +16,36 @@ public class EJ1 {
 
     public static void main(String[] args) {
         /*ctes*/
-        final int tamanoArray = 15;
-        /*Creo mi array*/
-        double[] arrayDoubles = new double[tamanoArray];
-        /*relleno todas las posiciones con el mismo valor*/
-        for (int i = 0; i < arrayDoubles.length; i++) {
-            arrayDoubles[i] = 7.5;
-        }
-        /*relizo una copia de mi array*/
-        double[] copiaArrayDoubles = arrayDoubles.clone();
-        /*muestro ambos por consola*/
-        System.out.println(Arrays.toString(arrayDoubles));
-        System.out.println("--------------------------------------");
-        System.out.println(Arrays.toString(copiaArrayDoubles));
-        System.out.println("--------------------------------------");
-        /*Comparo los arrays y devuelvo si son iguales*/
-        boolean arraysIguales = Arrays.equals(arrayDoubles, copiaArrayDoubles);
-        System.out.println(arraysIguales);
-        /*cambio el valor en una posicion aleatoria de mi array*/
-        Random random = new Random();
-        int arrayElegido = random.nextInt(1, 3);
-        if (arrayElegido == 1) {
-            int posAleatoria = random.nextInt(0, arrayDoubles.length);
-            arrayDoubles[posAleatoria] = 6.3;
+        final int TAMAÑO_ARRAY = 20;
+        /*creo mis dos arrays*/
+        int[] array1 = new int[TAMAÑO_ARRAY];
+        rellenaArray(array1);
+        System.out.println("Primer array: " + Arrays.toString(array1));
+        int[] array2 = new int[25];
+        rellenaArray(array2);
+        System.out.println("Segundo array: " + Arrays.toString(array2));
+        /*ordeno los arrays*/
+        System.out.println("------------------------------------------------------------------");
+        ordenaArray(array1);
+        ordenaArray(array2);
+        System.out.println("Primer array ordenado: " + Arrays.toString(array1));
+        System.out.println("Segundo array ordenado: " + Arrays.toString(array2));
+        System.out.println("------------------------------------------------------------------");
+        /*verifico si son iguales*/
+        boolean iguales = arraysIguales(array1, array2);
+        if (iguales == true) {
+            System.out.println("Los arrays son iguales");
         } else {
-            int posAleatoria = random.nextInt(0, copiaArrayDoubles.length);
-            copiaArrayDoubles[posAleatoria] = 6.3;
+            System.out.println("Los arrays son distintos");
         }
-        /*muestro ambos por consola*/
-        System.out.println(Arrays.toString(arrayDoubles));
-        System.out.println("----------------------------------------");
-        System.out.println(Arrays.toString(copiaArrayDoubles));
-        System.out.println("----------------------------------------");
-        /*Comparo los arrays y devuelvo si son iguales*/
-        arraysIguales = Arrays.equals(arrayDoubles, copiaArrayDoubles);
-        System.out.println(arraysIguales);
-        System.out.println("----------------------------------------");
-        /*Ej 2*/
-        /*genero un array con el metodo rellenaArray*/
-        int[] arrayDeNumAleatorios = new int[20];
-        rellenaArray(arrayDeNumAleatorios);
-        System.out.println("----------------------------------------");
-        System.out.println(Arrays.toString(arrayDeNumAleatorios));
-        /*Ordeno las posiciones de mi array*/
-        Arrays.sort(arrayDeNumAleatorios);
-        System.out.println(Arrays.toString(arrayDeNumAleatorios));
-        System.out.println("----------------------------------------");
+        System.out.println("--------------------------------------------------------------------");
         /*busco un numero especifico*/
-        Scanner teclado = new Scanner(System.in);
-        boolean errorNum = true;
-        int numEspecifico = 0;
-        do {
-            try {
-                System.out.println("Introduce un número entero");
-                numEspecifico = teclado.nextInt();
-                teclado.nextLine();/*limpio la variable en caso de que sea correcto*/
-                errorNum = false;
-            } catch (InputMismatchException ime) {
-                System.out.println("No has cumplido con los requisitos especificados");
-                errorNum = true;
-                teclado.nextLine();/*limpio la variable en caso de error*/
-            }
-        } while (errorNum);
         /*encuentra la posición del número especificado*/
-        int pos = Arrays.binarySearch(arrayDeNumAleatorios,numEspecifico);
-        if (pos >=0) {/*se ha decidido implementar con binary search*/
-            System.out.println(numEspecifico + " esta en la posición " + pos + " del array");
+        int pos = numSpecifico(array2);
+        if (pos >= 0) {/*se ha decidido implementar con binary search*/
+            System.out.println( "el numero introducido esta en la posicion "+pos+" del array");
         } else {
-            System.out.println(numEspecifico + " no se encuentra en el array");
+            System.out.println("EL numero introducido no se encuentra en el array");
         }
 
     }
@@ -96,35 +58,81 @@ public class EJ1 {
         }
         return array;
     }
-/*metodo que hace lo mismo que binarySearch*/
-//    public static int numSpecifico(int[] array, int num) {/* hace lo mismo que
-//        Arrays.binarySearch(array, num)*/
-//        /*posiciones*/
-//        int mitad = 0;
-//        int izquierda = 0;
-//        int derecha = array.length - 1;
-//        /*le indico el numero que deseo buscar*/
-//        int numABuscar = num;
-//        boolean numEncontrado = false;
-//        while ((izquierda <= derecha) && (!numEncontrado)) {
-//            mitad = (izquierda + derecha) / 2;
-//            if (array[mitad] == numABuscar) {
-//                numEncontrado = true;/*si encuentra la posicion, indica que la ha encontrado*/
-//            } else if (array[mitad] > numABuscar) {
-//                derecha = mitad - 1;
-//                /*buscar en el cuadrante izq del array*/
-//            } else {
-//                izquierda = mitad + 1;
-//                /*buscar en el cuadrante derecho del array*/
-//            }
-//        }
+
+    /*metodo que ordena el array mediante el metodo de ordenacion de la burbuja*/
+    public static void ordenaArray(int[] array) {
+        int aux;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    aux = array[i];
+                    array[i] = array[j];
+                    array[j] = aux;
+                }
+            }
+        }
+    }
+
+    /*metodo que mira si dos arrays son iguales*/
+    public static boolean arraysIguales(int[] array1, int[] array2) {
+        boolean arrayIgual = true;
+        if (array1.length != array2.length) {
+            arrayIgual = false;
+        } else {
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] != array2[i]) {
+                    arrayIgual = false;
+                }
+            }
+
+        }
+        return arrayIgual;
+    }
+
+    /*metodo que hace lo mismo que binarySearch*/
+    public static int numSpecifico(int[] array) {/* hace lo mismo que
+        Arrays.binarySearch(array, num)*/
+        Scanner teclado = new Scanner(System.in);
+        boolean errorNum = true;
+ /*posiciones*/
+        int mitad = 0;
+        int izquierda = 0;
+        int derecha = array.length - 1;
+        /*le indico el numero que deseo buscar*/
+        int numABuscar = 0;
+        
+        do {
+            try {
+                System.out.println("Introduce un número entero");
+                numABuscar = teclado.nextInt();
+                teclado.nextLine();/*limpio la variable en caso de que sea correcto*/
+                errorNum = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("No has cumplido con los requisitos especificados");
+                errorNum = true;
+                teclado.nextLine();/*limpio la variable en caso de error*/
+            }
+        } while (errorNum);
+        boolean numEncontrado = false;
+        while ((izquierda <= derecha) && (!numEncontrado)) {
+            mitad = (izquierda + derecha) / 2;
+            if (array[mitad] == numABuscar) {
+                numEncontrado = true;/*si encuentra la posicion, indica que la ha encontrado*/
+            } else if (array[mitad] > numABuscar) {
+                derecha = mitad - 1;
+                /*buscar en el cuadrante izq del array*/
+            } else {
+                izquierda = mitad + 1;
+                /*buscar en el cuadrante derecho del array*/
+            }
+        }
         /*si el número ha sido encontrado, devuelve la posición en la que esta*/
-        /*si no devuelve un valor por defecto, en este caso 504*/
-//        if (numEncontrado) {
-//            return mitad;
-//        } else {
-//            return 504;
-//        }
-//
-//    }
+ /*si no devuelve un valor por defecto, en este caso -1*/
+        if (numEncontrado) {
+            return mitad;
+        } else {
+            return -1;
+        }
+
+    }
 }
